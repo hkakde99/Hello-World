c160cd0f21ff9b34fc6773315747e40ee76f97aa
himanshu <hkakde104@gmail.com>
2021-04-27 13:24:45 +0530

    little in log message

--- src/main/java/annonymous/demo/ServiceImpl.java
+++ src/main/java/annonymous/demo/ServiceImpl.java
@@ -5,36 +5,36 @@ public class ServiceImpl {
 	Service s1 = new Service() {
 		@Override
 		public void modifyRecord() {
-			System.out.println("Implementation 1");
+			System.out.println("Implementation : 1");
 		}
 
 		@Override
 		public void deleteRecord() {
-			System.out.println("Implementation 1");
+			System.out.println("Implementation : 1");
 		}
 	};
 
 	Service s2 = new Service() {
 		@Override
 		public void modifyRecord() {
-			System.out.println("Implementation 2");
+			System.out.println("Implementation : 2");
 		}
 
 		@Override
 		public void deleteRecord() {
-			System.out.println("Implementation 2");
+			System.out.println("Implementation : 2");
 		}
 	};
 
 	Service s3 = new Service() {
 		@Override
 		public void modifyRecord() {
-			System.out.println("Implementation 3");
+			System.out.println("Implementation : 3");
 		}
 
 		@Override
 		public void deleteRecord() {
-			System.out.println("Implementation 3");
+			System.out.println("Implementation : 3");
 		}
 
 	};
